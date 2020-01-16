67d61cdff9e88e19bb95d43be0f0ef00c56a79b7
simoxsimo <touili.mohammed@gmail.com>
2020-01-16 20:04:25 +0000

    Fixed previous methods: Methods are working with Hashes too

--- main.rb
+++ main.rb
@@ -1,51 +1,77 @@
 module Enumerable
 	def my_each
-		i=-1
-		(self.size).times { 
-			i+=1
-			yield(self[i]) if block_given?
+		(self.size).times { |i|
+			yield(self[i]) if (block_given? && self.is_a?(Array))
+			yield(self.keys[i], self.values[i]) if (block_given? && self.is_a?(Hash))
 		}
 		self
 	end
 
 	def my_each_with_index
 		for i in (0..self.size-1)
-			yield(self[i], i) if block_given?
+			yield(self[i], i) if (block_given? && self.is_a?(Array))
+			yield(self.values[i], i) if (block_given? && self.is_a?(Hash))
 		end
 		self
 	end
 
 	def my_select
-		temp = []
-		i=0
-		self.my_each { |x|
-			if yield(x)
-				temp[i] = x
-				i+=1
-			end
-		}
-		temp
+		if self.is_a?(Array)
+			temp = []
+			i=0
+			self.my_each { |x|
+				if yield(x)
+					temp[i] = x
+					i+=1
+				end
+			}
+			temp
+		else #if self.is_a?(Hash)
+			temp = {}
+			self.my_each { |x, y| temp[x] = y if yield(x, y)}
+			temp
+		end
 	end
 
 	def my_all?
-		self.my_each{ |x| return false unless yield(x)}
-		true
+		if self.is_a?(Array)
+			self.my_each{ |x| return false unless yield(x)}  
+			true
+		else #if self.is_a?(Hash)
+			self.my_each{ |x, y| return false unless yield(x, y)}
+			true
+		end
 	end
 
 	def my_any?
-		self.my_each{ |x| return true if yield(x)}
-		false
+		if self.is_a?(Array)
+			self.my_each{ |x| return true if yield(x)}
+			false
+		else #if self.is_a?(Hash)
+			self.my_each{ |x, y| return true if yield(x, y)}
+			false
+		end
 	end
 
 	def my_none?
-		self.my_each { |x| return false if yield(x)}
-		true
+		if self.is_a?(Array)
+			self.my_each { |x| return false if yield(x)}
+			true
+		else #if self.is_a?(Hash)
+			self.my_each { |x, y| return false if yield(x, y)}
+			true
+		end
 	end
 
 	def my_count(test=nil)
 		counter = 0
-		self.my_each { |x| counter += 1 if ((x == test unless test.nil?) || (yield(x) if block_given?) )}
-		counter
+		if self.is_a?(Array)
+			self.my_each { |x| counter += 1 if ((x == test unless test.nil?) || (yield(x) if block_given?) )}
+			counter
+		else #if self.is_a?(Hash)
+			self.my_each { |x, y| counter += 1 if ((y == test unless test.nil?) || (yield(x, y) if block_given?) )} # when passing value per argument in the original method 'count' it doesn't work for example try this: "print hash_num.count(7)" it will return 0 but in my_count method it will count the number of 7
+			counter
+		end
 	end
 end
 
@@ -54,69 +80,114 @@ end
 
 
 include Enumerable
-# Test Array
-# => arr = [1,4,5,6,7,8,9]
+
+# Test 
+# 
+# Array => arr = [1,4,5,6,7,8,9]
+# Hash =>  hash_num = {one: 1, two: 2, nine: 9, seven: 7, four: 4}
 
 
 # Testing my_each VS each
+#
 # ***************************************************
+# *     ===============-Array Test-===============  *
 # *     print arr.my_each { |x| x*2 }.to_s + "\n"   *
 # *     puts"-----------------------"               *
 # *     print arr.each{|x| x*2}                     *
+# *     ===============-Hash Test-===============   *
+# *     print hash_num.each{|x, y| y*2}             *
+# *     puts"\n--------------"                      *   
+# *     print hash_num.my_each {|x, y| y*2}         *
 # ***************************************************
 
 # Testing my_each_with_index VS each_with_index
-# ***************************************************************
-# *     arr.my_each_with_index { |x, y| print y }               *
-# *     puts"\n-----------------------"                         *
-# *     arr.each_with_index{|x, y| print y}                     *
-# ***************************************************************
+#
+# ********************************************************
+# *     ===============-Array Test-===============       *
+# *     arr.my_each_with_index { |x, y| print y }        *
+# *     puts"\n-----------------------"                  *
+# *     arr.each_with_index{|x, y| print y}              *
+# *     ===============-Hash Test-===============        *
+# *     hash_num.my_each_with_index { |x, y| print y }   *
+# *     puts"\n-----------------------"                  *
+# *     hash_num.each_with_index{|x, y| print y}         *
+# ********************************************************
 
 # Testing my_select VS select
-# *****************************************
-# *     print arr.my_select{ |x| x>4 }    *
-# *     puts"\n-----------------------"   *
-# *     print arr.select{|x| x>4}         *
-# *****************************************
+#
+# ********************************************************
+# *     ===============-Array Test-===============       *
+# *     print arr.my_select{ |x| x>4 }                   *
+# *     puts"\n-----------------------"                  *
+# *     print arr.select{|x| x>4}                        *
+# *     ===============-Hash Test-===============        *
+# *     print hash_num.my_select{|x, y| y>4}             *
+# *     puts"\n-----------------------"                  *
+# *     print hash_num.select{|x, y| y>4}                *
+# ********************************************************
 
 # Testing my_all? VS all?
-# *****************************************
-# *     print arr.all?{ |x| x<=9 }        *
-# *     puts"\n-----------------------"   *
-# *     print arr.my_all?{|x| x<=9}       *
-# *****************************************
+#
+# ********************************************************
+# *     ===============-Array Test-===============       *
+# *     print arr.all?{ |x| x<=9 }                       *
+# *     puts"\n-----------------------"                  *
+# *     print arr.my_all?{|x| x<=9}                      *
+# *     ===============-Hash Test-===============        *
+# *     print hash_num.my_all?{|x, y| y.is_a? Integer}   *
+# *     puts"\n-----------------------"                  *
+# *     print hash_num.all?{|x, y| y.is_a? Integer}      *
+# ********************************************************
 
 # Testing my_any? VS any?
-# *****************************************
-# *     print arr.any?{ |x| x>9 }         *
-# *     puts"\n-----------------------"   *
-# *     print arr.my_any?{|x| x>9}        *
-# *****************************************
+#
+# ********************************************************
+# *     ===============-Array Test-===============       *
+# *     print arr.any?{ |x| x>9 }                        *
+# *     puts"\n-----------------------"                  *
+# *     print arr.my_any?{|x| x>9}                       *
+# *     ===============-Hash Test-===============        *
+# *     print hash_num.my_any?{|x, y| y>1}               *
+# *     puts"\n-----------------------"                  *
+# *     print hash_num.any?{|x, y| y>1}                  *
+# ********************************************************
 
 # Testing my_none? VS none?
-# *****************************************
-# *     print arr.none?{ |x| x>9 }        *
-# *     puts"\n-----------------------"   *
-# *     print arr.my_none?{|x| x>9}       *
-# *****************************************
+#
+# ********************************************************
+# *     ===============-Array Test-===============       *
+# *     print arr.none?{ |x| x>9 }                       *
+# *     puts"\n-----------------------"                  *
+# *     print arr.my_none?{|x| x>9}                      *
+# *     ===============-Hash Test-===============        *
+# *     print hash_num.my_none?{|x, y| y.is_a? String}   *
+# *     puts"\n-----------------------"                  *
+# *     print hash_num.none?{|x, y| y.is_a? String}      *
+# ********************************************************
 
 # Testing my_count VS count
-# *****************************************
-# *     print arr.count{ |x| x.even? }    *
-# *     puts"\n-----------------------"   *
-# *     print arr.my_count{|x| x.even?}   *
-# *     puts"\n$$$$$$$$$$$$$$$$$$$$$$$"   *
-# *     print arr.count(7)                *
-# *     puts"\n-----------------------"   *
-# *     print arr.my_count(7)             *
-# *****************************************
+#
+# ********************************************************
+# *     ===============-Array Test-===============       *
+# *     print arr.count{ |x| x.even? }                   *
+# *     puts"\n-----------------------"                  *
+# *     print arr.my_count{|x| x.even?}                  *
+# *     puts"\n$$$$$$$$$$$$$$$$$$$$$$$"                  *
+# *     print arr.count(7)                               *
+# *     puts"\n-----------------------"                  *
+# *     print arr.my_count(7)                            *
+# *     ===============-Hash Test-===============        *
+# *     print hash_num.my_count{|x, y| y.odd?}           *
+# *     puts"\n-----------------------"                  *
+# *     print hash_num.count{|x, y| y.odd?}              *
+# *     puts"\n$$$$$$$$$$$$$$$$$$$$$$$"                  *
+# *     print hash_num.my_count{|x, y| x==:one}          *
+# *     puts"\n-----------------------"                  *
+# *     print hash_num.count{|x, y| x==:one}             *
+# ********************************************************
 
-arr = [1,4,5,6,7,8,9]	
-print arr.count{ |x| x.even? }
-puts"\n-----------------------"
-print arr.my_count{|x| x.even?}
-puts"\n$$$$$$$$$$$$$$$$$$$$$$$"
-arr = ['a','a','b','c','a']
-print arr.count("a")
-puts"\n-----------------------"
-print arr.my_count("a")
+hash_num = {one: 1, two: 2, nine: 9, seven: 7, four: 4}
+arr = [1,4,5,6,7,8,9]
+print hash_num.my_count(7) 
+puts"\n-----------------------"          
+print hash_num.count(7)     
